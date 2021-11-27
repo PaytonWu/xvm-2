45a7908d407f0b602c6f78708a71bb2094b2934b
payton.wu <payton.wu@topnetwork.org>
2021-11-27 12:42:50 +0800

    feat(xvm): update fork logic

--- manager/xrole_context.cpp
+++ manager/xrole_context.cpp
@@ -45,6 +45,7 @@ void xrole_context_t::on_block_to_db(const xblock_ptr_t & block, bool & event_br
     if (chain_upgrade::xtop_chain_fork_config_center::is_forked(fork_config.table_statistic_info_fork_point, block->get_clock())) {
         // process block event
         if (m_contract_info->has_block_monitors()) {
+            assert(false);
             auto block_owner = block->get_block_owner();
             // table fulltable block process
             if ((m_contract_info->address == common::xaccount_address_t{sys_contract_sharding_statistic_info_addr}) &&
@@ -152,10 +153,11 @@ void xrole_context_t::on_block_timer(const xevent_ptr_t & e) {
         }
 
         auto const & fork_config = top::chain_upgrade::xtop_chain_fork_config_center::chain_fork_config();
-        if (chain_upgrade::xchain_fork_config_center_t::is_forked(fork_config.new_system_contract_runtime_fork_point, block->get_height())) {
-            return;
-        }
+        //if (chain_upgrade::xchain_fork_config_center_t::is_forked(fork_config.new_system_contract_runtime_fork_point, block->get_height())) {
+        //    return;
+        //}
         if (info != nullptr) {
+            assert(false);
             bool do_call{false};
             uint64_t block_timestamp{0};
             uint64_t onchain_timer_round{0};
